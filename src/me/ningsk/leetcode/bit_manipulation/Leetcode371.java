aedc5f37039b8b83bf0b08013f4ba6ae6091d4a7
宁四凯 <ningsk0115@hotmail.com>
2021-09-27 16:12:52 +0800

    位运算学习

---
+++ src/me/ningsk/leetcode/bit_manipulation/Leetcode371.java
@@ -0,0 +1,36 @@
+package me.ningsk.leetcode.bit_manipulation;
+
+/**
+ * 两个整数之和
+ * 给你两个整数 a 和 b ，不使用 运算符+ 和-，计算并返回两整数之和。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：a = 1, b = 2
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：a = 2, b = 3
+ * 输出：5
+ *  
+ *
+ * 提示：
+ *
+ * -1000 <= a, b <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-two-integers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leetcode371 {
+    public static void main(String[] args) {
+        Leetcode371 solution = new Leetcode371();
+        int result = solution.getSum(45, -9);
+        System.out.println(result);
+    }
+    public int getSum(int a, int b) {
+        return 0;
+    }
+}
